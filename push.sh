4a8a6361a529061768db2d307af26f0e22a04c50
Diogo <diogobezerra5@gmail.com>
2026-04-22 20:29:36 -0300

    update

--- push.sh
+++ push.sh
@@ -6,12 +6,12 @@ git add .
 git commit -m "$commit" || echo "⚠️ Nenhuma alteração para commitar"
 git push -u origin "$branch" || exit 1
 
-# Obs: Como estou desenvolvendo sozinho aqui dou push direto pra dev pra não ter que executar 
-# também pushdev.sh. 
-# dev tem que estar sempre atualizada pois é de onde são criadas novas branches de desenvolvimento
-
+# Obs: 
 # No caso de mais de um desenvolvedor deve-se mandar pra dev (pushdev.sh) somente 
 # quando a feature ou correção estiver pronta, revisada e autorizada
+# Como estou desenvolvendo sozinho aqui dou push direto pra dev pra não ter que executar 
+# também pushdev.sh. 
+# dev tem que estar sempre atualizada pois é de onde são criadas novas branches de desenvolvimento
 git checkout dev 
 git merge $branch
 git push
